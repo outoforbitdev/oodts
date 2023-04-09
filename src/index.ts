339ead14bcb6153de6c724233202985681fafd27
Jay Mirecki <jarett.mirecki@gmail.com>
2023-04-08 22:00:14 -0500

    feat(string): add isNullOrEmpty

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { curry, curryAndBind, bind } from "./curry";
 import { isMobile } from "./webpage";
+import { isNullOrEmpty } from "./string";
 
 export {
     curry,
     curryAndBind,
     bind,
     isMobile,
+    isNullOrEmpty,
 }
